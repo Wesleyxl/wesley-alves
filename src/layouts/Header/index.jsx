a31bc74e5996b479774e3d6e7788385747c3225a
Wesleyxl <wesley.alvesxll@gmail.com>
2022-10-25 22:47:14 -0300

    header done

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import WLogo from "../../assets/W-icon.svg";
-import { Container } from "./styles";
+import { Container, DesktopMenu, MobileMenu, SocialMedias } from "./styles";
 
 function Header() {
+  function handleMenuMobile() {
+    const element = document.querySelector("#mobile-menu-handler");
+    if (element) {
+      if (element.classList.contains("inactive")) {
+        element.classList.remove("inactive");
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+        element.classList.add("inactive");
+      }
+    }
+  }
+
   return (
     <Container>
-      <nav>
+      <DesktopMenu>
         <ul>
           <li>
             <a href="/" className="inactive">
@@ -38,13 +51,75 @@ function Header() {
             </NavLink>
           </li>
           <li>
-            <select name="lang" id="lang">
+            <select name="lang" id="lang" defaultValue="pt">
               <option value="en">EN</option>
               <option value="pt">PT</option>
             </select>
           </li>
         </ul>
-      </nav>
+      </DesktopMenu>
+
+      <MobileMenu>
+        <ul>
+          <li>
+            <a href="/" className="inactive">
+              <img src={WLogo} alt="Wesley Alves" />
+            </a>
+          </li>
+          <li>
+            <button type="button" onClick={handleMenuMobile}>
+              <i className="fa-solid fa-bars" />
+            </button>
+          </li>
+        </ul>
+        <nav id="mobile-menu-handler" className="inactive">
+          <li>
+            <NavLink to="/" alt="Home" title="Home">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/work" alt="Home" title="Home">
+              Work
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/project" alt="Home" title="Home">
+              Project
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" alt="Home" title="Home">
+              Contact
+            </NavLink>
+          </li>
+        </nav>
+      </MobileMenu>
+
+      <SocialMedias>
+        <ul>
+          <div className="line" />
+          <li>
+            <a
+              href="https://github.com/Wesleyxl"
+              alt="Github Wesley Alves"
+              title="Github Wesley Alves"
+            >
+              <i className="fa-brands fa-github" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              alt="LinkedIn Wesley Alves"
+              title="LinkedIn Wesley Alves"
+            >
+              <i className="fa-brands fa-linkedin" />
+            </a>
+          </li>
+          <div className="line" />
+        </ul>
+      </SocialMedias>
     </Container>
   );
 }
--- src/layouts/Header/styles.js
+++ src/layouts/Header/styles.js
@@ -2,79 +2,48 @@ import styled from "styled-components";
 
 const Container = styled.header`
   width: 100%;
-  height: 80px;
   position: fixed;
   top: 0;
   left: 0;
 
-  nav {
-    width: 100%;
-    height: 100%;
-    max-width: 1300px;
-    margin: 0 auto;
+  @media (max-width: 767px) {
+    border-bottom: 2px solid #ffffff;
+  }
+`;
+
+const DesktopMenu = styled.div`
+  width: 100%;
+  height: 80px;
+  max-width: 1300px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+
+  ul {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
 
-    ul {
-      display: flex;
+    li {
+      margin-right: 15px;
+      list-style: none;
 
-      li {
-        margin-right: 15px;
-        list-style: none;
+      img {
+        width: 26px;
+      }
 
-        img {
-          width: 26px;
-        }
+      a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 20px;
+        position: relative;
 
-        a {
-          color: #fff;
-          text-decoration: none;
-          font-size: 20px;
-          position: relative;
-
-          :hover {
-            font-weight: bold;
-
-            :before {
-              transform: scale3d(1, 1, 1);
-            }
-            :after {
-              transform: scale3d(1, 1, 1);
-            }
-          }
-
-          ::before {
-            content: "";
-            position: absolute;
-            bottom: -3px;
-            left: 0;
-            width: 100%;
-            height: 4px;
-            border-radius: 2px;
-            background: #fff;
-            transform: scale3d(0, 1, 1);
-            transform-origin: center;
-            transition: ease 0.8s;
-          }
-
-          ::after {
-            content: "";
-            position: absolute;
-            bottom: -10px;
-            left: 25%;
-            width: 50%;
-            height: 4px;
-            border-radius: 2px;
-            background: #fff;
-            transform: scale3d(0, 1, 1);
-            transform-origin: center;
-            transition: ease 0.8s;
-          }
-        }
-
-        .active {
+        :hover {
           font-weight: bold;
+
           :before {
             transform: scale3d(1, 1, 1);
           }
@@ -83,37 +52,75 @@ const Container = styled.header`
           }
         }
 
-        .inactive {
-          font-weight: normal;
-
-          :hover {
-            :before {
-              transform: scale3d(0, 1, 1);
-            }
-            :after {
-              transform: scale3d(0, 1, 1);
-            }
-          }
+        ::before {
+          content: "";
+          position: absolute;
+          bottom: -3px;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          border-radius: 2px;
+          background: #fff;
+          transform: scale3d(0, 1, 1);
+          transform-origin: center;
+          transition: ease 0.8s;
         }
 
-        select {
-          margin-left: 30px;
-          width: 70px;
-          height: 30px;
-          background: transparent;
-          color: #fff;
-          padding: 5px;
-          outline: none;
+        ::after {
+          content: "";
+          position: absolute;
+          bottom: -10px;
+          left: 25%;
+          width: 50%;
+          height: 4px;
+          border-radius: 2px;
+          background: #fff;
+          transform: scale3d(0, 1, 1);
+          transform-origin: center;
+          transition: ease 0.8s;
+        }
+      }
+
+      .active {
+        font-weight: bold;
+        :before {
+          transform: scale3d(1, 1, 1);
+        }
+        :after {
+          transform: scale3d(1, 1, 1);
+        }
+      }
+
+      .inactive {
+        font-weight: normal;
+
+        :hover {
+          :before {
+            transform: scale3d(0, 1, 1);
+          }
+          :after {
+            transform: scale3d(0, 1, 1);
+          }
+        }
+      }
+
+      select {
+        margin-left: 30px;
+        width: 70px;
+        height: 30px;
+        background: transparent;
+        color: #fff;
+        padding: 5px;
+        outline: none;
+
+        :hover {
+          cursor: pointer;
+        }
+        option {
+          color: #000;
 
           :hover {
             cursor: pointer;
-          }
-          option {
-            color: #000;
-
-            :hover {
-              cursor: pointer;
-            }
           }
         }
       }
@@ -121,4 +128,89 @@ const Container = styled.header`
   }
 `;
 
-export { Container };
+const MobileMenu = styled.div`
+  width: 100%;
+  height: 80px;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+
+  ul {
+    padding: 15px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    list-style-type: none;
+
+    img {
+      width: 50px;
+    }
+
+    button {
+      border: none;
+      background: transparent;
+      font-size: 36px;
+      color: #fff;
+    }
+  }
+
+  nav {
+    position: absolute;
+    width: 80%;
+    height: calc(100vh - 80px);
+    padding: 15px 15px 15px 40px;
+    background: #282c33;
+    border-right: 2px solid #fff;
+    transition: ease 0.5s;
+
+    li {
+      list-style: none;
+      margin: 40px 0;
+
+      a {
+        color: #fff;
+        font-size: 36px;
+        text-decoration: none;
+      }
+    }
+  }
+  .active {
+    left: 0;
+  }
+
+  .inactive {
+    left: -1500px;
+  }
+`;
+
+const SocialMedias = styled.div`
+  position: fixed;
+  left: 40px;
+  top: 25%;
+
+  .line {
+    width: 2px;
+    height: 150px;
+    background: #fff;
+    margin: auto;
+  }
+
+  li {
+    list-style: none;
+    margin: 10px 0;
+
+    a {
+      color: #fff;
+      font-size: 22px;
+    }
+  }
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+export { Container, MobileMenu, DesktopMenu, SocialMedias };
